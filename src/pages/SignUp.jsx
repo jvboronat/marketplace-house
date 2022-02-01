2ae2f83ec3f785f8c69f0fc2a362e457c049c4e9
Jose Vte. Boronat <jvboronat@gmail.com>
2022-02-01 08:47:36 +0100

    cleaning up: navigate not needed

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -9,9 +9,7 @@ function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({email: '', password: '', name: ''});
 
-  const {email, password, name} = formData
-
-  const navigate = useNavigate()
+  const {email, password, name} = formData  
 
   const onChange = (e) => {
     setFormData((prevState) => ({
