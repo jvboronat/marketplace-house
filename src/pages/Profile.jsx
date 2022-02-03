79480eb70b8ab55c03c53a6987dc69ff02a2abda
Jose Vte. Boronat <jvboronat@gmail.com>
2022-02-03 08:50:32 +0100

    Sign UP, Save user on DB, Sign IN

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,22 @@
+import {getAuth} from 'firebase/auth'
+
+import {useEffect, useState} from 'react'
+
 function Profile() {
-    return <div>Profile</div>;
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth()
+
+    console.log(auth.currentUser)
+
+    setUser(auth.currentUser)
+
+  }, [])
+
+  return user ? <h1>{user.displayName}</h1>: 'Not logged In'
+  
   }
   
   export default Profile;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,8 @@
 import {useState} from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {db} from '../firebase.config'
+import {setDoc, doc, serverTimestamp} from 'firebase/firestore'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -11,11 +14,46 @@ function SignUp() {
 
   const {email, password, name} = formData  
 
+  const navigate = useNavigate()
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value
     }) )
+
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      // Save user in DB
+
+      // Prepare object
+      const formDataCopy = {...formData}
+      delete formDataCopy.password
+      formDataCopy.serverTimestamp = serverTimestamp()
+
+      // Save on DB
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+      navigate('/')
+
+    } catch (error) {
+      
+      console.log(error);
+    }
 
   }
 
@@ -25,7 +63,7 @@ function SignUp() {
             <p className="pageHeader">Welcome Back!</p>
           </header>
           <main>
-            <form>
+            <form onSubmit={onSubmit}>
               <input type="text" className="nameInput" placeholder='Name' 
                 value={name} id="name" onChange={onChange}/>
 
